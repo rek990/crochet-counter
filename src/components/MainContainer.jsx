c1a90bd1e885a2cc546d7c159f9f438ae522ffb6
Rebecca Key <rek990@gmail.com>
2022-07-07 16:39:15 -0400

    Created component to hold the row input and counter.

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Flex } from "@chakra-ui/react";
 
 const MainContainer = () => {
-    return <></>;
+    return <>
+         <Flex
+      position="absolute"
+      bg="rgba(53, 98, 136, 0.6)"
+      height="55%"
+      width="75vw"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {/* <RowCounterContainer/> */}
+    </Flex>
+    </>;
 }
 
 export default MainContainer;
