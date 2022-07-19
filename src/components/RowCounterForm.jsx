d4886009a5a0fe6cd563121abf34718a0eb8829e
Rebecca Key <rek990@gmail.com>
2022-07-19 15:13:58 -0400

    Removed unnecessary code and comments.

--- src/components/RowCounterForm.jsx
+++ src/components/RowCounterForm.jsx
@@ -3,125 +3,25 @@ import {
   Grid,
   GridItem,
   Input,
-  Select,
-  Textarea,
   Button,
   FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Heading,
   Flex,
   HStack,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
 } from "@chakra-ui/react";
 
 const RowCounterForm = () => {
-  // let [qsoNumber, setQsoNumber] = useState(6);
-
-  // const [qsoDate, setQsoDate] = useState("");
-  // const [qsoTime, setQsoTime] = useState("");
-  // const [meterBand, setMeterBand] = useState(null);
-  // const [modeNames, setModeNames] = useState(null);
-  // const [frequencyValues, setFrequencyValues] = useState(null);
-  // const [power, setPower] = useState("");
-  // const [signalSent, setSignalSent] = useState("");
-  // const [signalReceived, setSignalReceived] = useState("");
-  // const [operatorName, setOperatorName] = useState("");
-  // const [country, setCountry] = useState("");
-  // const [id, setId] = useState(0);
-  // const [qsoData, setQsoData] = useState([]);
-
-  /*const getData = useCallback(async () => {
-      const resp = await fetch("http://localhost:3000/qsoHistory");
-      const data = await resp.json();
-      const loadedQSOs = [];
-      for (const key in data) {
-        loadedQSOs.push({
-          id: key,
-          qsoNumber: data[key].qsoNumber,
-          qsoDate: data[key].qsoDate,
-          qsoTime: data[key].qsoTime,
-          band: data[key].band,
-          frequency: data[key].frequency,
-          mode: data[key].mode,
-          notes: data[key].notes,
-        });
-      }
-      console.log(data);
-      //setCells(data);
-      setCells(loadedQSOs);
-    });*/
-
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   const qsoDataPerContact = {
-
-  //     callSign: callSign,
-  //     qsoDate: qsoDate,
-  //     qsoTime: qsoTime,
-  //     band: meterBand,
-  //     mode: modeNames,
-  //     frequency: frequencyValues,
-  //     notes: notes,
-  //     qsoNumber: qsoNumber,
-  //   };
-  //   console.log(qsoDataPerContact.notes);
-
-  //   const response = await fetch("http://localhost:3000/qsoHistory", {
-  //     method: "POST",
-  //     credentials: "include",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(qsoDataPerContact),
-  //   });
-  //   const data = await response.json();
-  //   console.log(data);
-  //   setQsoData(qsoDataPerContact);
-  //   console.log(qsoNumber);
-  //   setQsoNumber(qsoNumber + 1);
-  //   console.log(qsoNumber);
-  //   setCallSign("");
-  //   setQsoDate("");
-  //   setQsoTime("");
-  //   setPower("");
-  //   setSignalSent("");
-  //   setSignalReceived("");
-  //   setRenderedNotes(notes);
-  //   console.log("renderedNotes", renderedNotes);
-  //   setNotes("");
-  //   console.log(renderedNotes);
-  //   setMeterBand("");
-  //   setModeNames("");
-  //   setFrequencyValues("");
-  // };
-
-  // useEffect(() => {
-  //   handleSubmit();
-  // }, []);
-  // console.log(qsoData);
-
   const [enterRowNumber, setEnterRowNumber] = useState("");
   const [displayRowNumber, setDisplayRowNumber] = useState(enterRowNumber);
   const [savedRowNumber, setSavedRowNumber] = useState("");
 
   const handleSubmit = () => {
     setDisplayRowNumber(enterRowNumber); // similar to document.getElementById("row-heading-number").innerText = enterRowNumber;
-    console.log("displayRowNumberSubmit", displayRowNumber);
-    console.log(typeof displayRowNumber);
-    console.log(typeof displayRowNumber);
     setEnterRowNumber("");
   };
 
   const handleIncrementor = () => {
     setDisplayRowNumber(+displayRowNumber + 1);
-    console.log("displayRowNumberIncrement", displayRowNumber);
   };
 
   const handleDecrementor = () => {
@@ -130,16 +30,11 @@ const RowCounterForm = () => {
     } else {
       setDisplayRowNumber(0);
     }
-    console.log("displayRowNumberIncrement", displayRowNumber);
   };
 
   const saveRowCount = () => {
-    // click Save button
     // this will eventually become a POST request (start with JSON server and take it from there)
-    // for now, log the saved row count to the console
-    // setSavedRowNumber(displayRowNumber);
     console.log("displayRowNumberSaved", displayRowNumber);
-    // reset the row counter to zero
     setDisplayRowNumber("");
   };
 
@@ -147,8 +42,6 @@ const RowCounterForm = () => {
     // click Resume button
     // this will eventually become a GET request (start with JSON server and take it from there)
     // display the saved count into the UI
-    // setSavedRowNumber(displayRowNumber);
-    // console.log("savedRowNumber", savedRowNumber);
     document.getElementById("resume-row-count").innerText = displayRowNumber;
   };
 
@@ -161,7 +54,6 @@ const RowCounterForm = () => {
           templateRows="repeat(2, 1fr)"
           templateColumns="repeat(8, 1fr)"
           gap={5}
-          // height={1}
           justifyContent="center"
           alignItems="center"
           textAlign="left"
@@ -194,7 +86,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={handleSubmit}
               >
                 Submit Entry
@@ -205,29 +96,14 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={() => setEnterRowNumber("")}
               >
                 Reset Entry
               </Button>
             </HStack>
           </GridItem>
-          <GridItem id="reset-row-input" colSpan={2}>
-            {/* <Button
-              id="reset-row-button"
-              type="reset"
-              size="sm"
-              bg="#5F9EA0"
-              color="white"
-              // float="right"
-              // onClick={handleSubmit}
-            >
-              Reset Entry
-            </Button> */}
-          </GridItem>
           <GridItem id="row-number-label" colSpan={2} colStart={4}>
             <Flex
-              // width="30vw"
               height="5vh"
               bg="#5F9EA0"
               borderRadius="15px"
@@ -245,20 +121,6 @@ const RowCounterForm = () => {
               ></Heading>
             </Flex>
           </GridItem>
-          {/* <GridItem id="row-number-output">
-            <Flex
-              height="5vh"
-              borderRadius="15px"
-              border="5px solid #5F9EA0"
-              justifyContent="center"
-              alignItems="center"
-              color="white"
-            >
-              <Heading size="lg" flex={1} textAlign="center">
-                6
-              </Heading>
-            </Flex>
-          </GridItem> */}
           <GridItem id="stitch-counter-buttons">
             <HStack>
               <Button
@@ -267,7 +129,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={handleIncrementor}
               >
                 +
@@ -278,7 +139,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={handleDecrementor}
               >
                 -
@@ -289,7 +149,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={saveRowCount}
               >
                 Save
@@ -300,7 +159,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={resumeRowCount}
               >
                 Resume
@@ -311,7 +169,6 @@ const RowCounterForm = () => {
                 size="sm"
                 bg="#5F9EA0"
                 color="white"
-                // float="right"
                 onClick={() => setDisplayRowNumber("")}
               >
                 Reset Counter
