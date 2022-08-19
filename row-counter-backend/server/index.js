b74c8a16f3a213e86f0f401ac4f07591cf88e351
Rebecca Key <rek990@gmail.com>
2022-08-19 17:56:21 -0400

    WIP: building routes for backend.

--- row-counter-backend/server/index.js
+++ row-counter-backend/server/index.js
@@ -2,28 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const MongoClient = require("mongodb").MongoClient;
+const db = require("./db");
+// const MongoClient = require("mongodb").MongoClient;
 const apiPort = 5000; // formerly 3000 (3000 currently in use with JSON Server)
 require("dotenv").config();
 
-let db,
-  dbConnectionStr = process.env.DB_STRING,
-  dbName = "fiber-art";
+// let db,
+//   dbConnectionStr = process.env.DB_STRING,
+//   dbName = "fiber-art";
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
-  (client) => {
-    console.log(`Connected to ${dbName} Database`);
-    db = client.db(dbName);
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+// (client) => {
+// console.log(`Connected to ${dbName} Database`);
+// db = client.db(dbName);
 
-    app.use(express.static("public"));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors());
-    app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-    app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
-  }
-);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+//   }
+// );
