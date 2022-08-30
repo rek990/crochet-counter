210c9dc004617cf1866f8f178201187d09e89779
Rebecca Key <rek990@gmail.com>
2022-08-30 17:21:21 -0400

    Moved the forward slash to the end of the route on row-counter.

--- row_counter_backend/row_counter_backend/urls.py
+++ row_counter_backend/row_counter_backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/row-counter', include('rctr.urls')),
+    path('row-counter/', include('rctr.urls')),
     path('admin/', admin.site.urls),
 ]
