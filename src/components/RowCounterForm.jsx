4413e9644978273c3928ce4b6ea64eac8213095d
Rebecca Key <rek990@gmail.com>
2023-01-10 15:50:57 -0500

    remove unneeded code

--- src/components/RowCounterForm.jsx
+++ src/components/RowCounterForm.jsx
@@ -27,18 +27,6 @@ const RowCounterForm = () => {
 
   const API_URL = "http://localhost:8000/row-counter/";
 
-  // const handleSubmit = () => {
-  //   setDisplayRowNumber(enterRowNumber);
-  //   setDisplayProjectName(enterProjectName);
-  //   setEnterRowNumber("");
-  //   setEnterProjectName("");
-  // };
-
-  // const handleResetEntry = () => {
-  //   setEnterRowNumber("");
-  //   setEnterProjectName("");
-  // };
-
   const handleResetAll = () => {
     setDisplayRowNumber("");
     setDisplayProjectName("");
@@ -187,65 +175,6 @@ const RowCounterForm = () => {
             setEnterRowNumber={setEnterRowNumber}
             setEnterProjectName={setEnterProjectName}
           />
-          {/* <GridItem id="project-name-form" colSpan={2} colStart={3}>
-            <Input
-              id="project-name-input"
-              placeholder="Enter Project Name"
-              size="sm"
-              borderRadius="6px"
-              border="3px solid"
-              borderColor="#5F9EA0"
-              bg="white"
-              focusBorderColor="#A05F9E"
-              errorBorderColor="#FE1100"
-              value={enterProjectName}
-              onChange={(event) => {
-                setEnterProjectName(event.target.value);
-              }}
-            ></Input>
-          </GridItem>
-          <GridItem id="row-counter-form" colSpan={2}>
-            <Input
-              id="row-number-input"
-              placeholder="Enter Row Number"
-              size="sm"
-              borderRadius="6px"
-              border="3px solid"
-              borderColor="#5F9EA0"
-              bg="white"
-              focusBorderColor="#A05F9E"
-              errorBorderColor="#FE1100"
-              value={enterRowNumber}
-              onChange={(event) => {
-                setEnterRowNumber(Number(event.target.value));
-              }}
-            ></Input>
-          </GridItem>
-
-          <GridItem id="submit-reset-buttons" colSpan={2}>
-            <HStack>
-              <Button
-                id="submit-row-button"
-                type="submit"
-                size="sm"
-                bg="#5F9EA0"
-                color="white"
-                onClick={handleSubmit}
-              >
-                Submit Entry
-              </Button>
-              <Button
-                id="reset-row-button"
-                type="reset"
-                size="sm"
-                bg="#5F9EA0"
-                color="white"
-                onClick={handleResetEntry}
-              >
-                Reset Entry
-              </Button>
-            </HStack>
-          </GridItem> */}
           {/* Insert <InteractiveProjectForm/> here */}
           <GridItem id="retrieve-project-form" colSpan={2}>
             <Input
